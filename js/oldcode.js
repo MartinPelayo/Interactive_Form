32913db507b7abf709dade7987c58917603204ef
Martin <martinconradpelayo@gmail.com>
2017-07-14 14:31:15 -0700

    commit

--- js/oldcode.js
+++ js/oldcode.js
@@ -23,8 +23,7 @@ $('#title').change(function(){
   }
 });
 
-//Hides and changes shirt values
-$(" option[value='cornflowerblue']").hide(); 
+$(" option[value='cornflowerblue']").hide(); //Hides and changes shirt values 
 $(" option[value='darkslategrey']").hide();
 $(" option[value='gold']").hide();  
 $(" option[value='tomato']").hide(); 
@@ -61,8 +60,7 @@ $('#design').change(function(){
   }
 });
 
-//click handlers for disabling/enabling event options
-$(jsFrameworks).click(function(e) { 
+$(jsFrameworks).click(function(e) { //click handlers for disabling/enabling event options
   var target = $(e.target);
   if (target.is(':checked') ) {
     $(express).prop({
@@ -113,9 +111,8 @@ $(node).click(function(e) {
     });
   }
 });
-
-//Calculates event total costs for event options
-$("input[type='checkbox']").click(function(e) { 
+  
+$("input[type='checkbox']").click(function(e) { //Calculates event total costs
   var target = $(e.target);
   if (target.is(':checked') ) {
     total += 100;
@@ -130,15 +127,14 @@ $("input[type='checkbox']").click(function(e) {
   }
   $('.total').text('$' + total);
 });
-
+ 
 // requirements option section requirements
 $("option[value='select_method']").hide();
 $("p:contains('the PayPal option')").hide();
 $("p:contains('the Bitcoin option')").hide();
 $('#credit-card').hide();
 
-//Shows payment options on click
-$('#payment').change(function(){
+$('#payment').change(function(){ //Shows payment options on click
   let value = $(this).val();
   if(value === 'paypal'){
     $("p:contains('the PayPal option')").show();
@@ -175,177 +171,206 @@ $('#payment').change(function(){
   }
 });
 
-//Handler checks all required info sections of form when
-//the submit button is clicked
-$("button:contains('Register')").click(function(e){
-  e.preventDefault();         
-  let nameInput = $('#name').val(); 
-  if (nameInput == ''){
-    $("button:contains('Register')").prop({
-      disabled: true
-    });
-    $('#name').css('border-color', '#c13c24');
-  }else{
-    $('#name').css('border-color', 'white');
-  }
 
-  let emailInput = $('#mail').val(); 
-  let aIndex = emailInput.indexOf('@');
-  let pIndex = emailInput.lastIndexOf('.');
-  if (aIndex < 1 || pIndex < aIndex +2 || pIndex +2 >= emailInput.length) {
-    $('#mail').css('border-color', '#c13c24');
-    $("button:contains('Register')").prop({
-      disabled: true
-    });
-  }
-  if (mainConf.is(':checked') || jsFrameworks.is(':checked') || jsLibs.is(':checked') 
-   || express.is(':checked') || node.is(':checked') || buildTools.is(':checked') || npm.is(':checked')){
-    $('legend span').text('').css('color', '#c13c24');
-    $("button:contains('Register')").prop({
-      disabled: true
-    });
-  }else{
-    $('legend span').text(' Must Choose at Least 1').css('color', '#c13c24');
-  }
-
-  if($('.default').is(':selected')){
-    $('label span').text(' Must Enter Payment Option').css('color', '#c13c24');   
-    $("button:contains('Register')").prop({
-      disabled: true
-    });
-  }
-
-
-  if($('.card').is(':selected')){
-    let val = $('#cc-num').val();
-    let valLength = val.length;
-    if (isNaN(val) || val === '' || valLength < 13 || valLength > 16){
-      $('#cc-num').css('border-color', '#c13c24');
-      $("button:contains('Register')").prop({
-        disabled: true
-      });
-    }else if(valLength > 13 && valLength < 16 && isNaN(val) === false){
-    $('#cc-num').css('border-color', 'white');
-  }
-  }
-
-  let zipVal = $('#zip').val();
-  let zipValLength = zipVal.length;
-  if (isNaN(zipVal) || zipValLength != 5){
-    $('#zip').css('border-color', '#c13c24');
-  }else if(zipValLength === 5 && isNaN(zipVal) === false) {
-    $('#zip').css('border-color', 'white');
-  }
-
-  let cvvVal = $('#cvv').val();
-  let cvvValLength = cvvVal.length;
-  if (isNaN(cvvVal) || cvvValLength != 3){
-    $('#cvv').css('border-color', '#c13c24');
-  }else if(cvvValLength === 3 && isNaN(cvvVal) === false) {
-    $('#cvv').css('border-color', 'white');
-  }
-
-
+//ERROR validation requirements \\
+$('#mail').prop({
+  disabled: true
+});
+$('.activities').prop({
+  disabled: true
+});
+$('.payment').prop({
+  disabled: true
+});
+$('#cc-num').prop({
+  disabled: true
+});
+$('#zip').prop({
+  disabled: true
+});
+$('#cvv').prop({
+  disabled: true
+});
+$("button:contains('Register')").prop({
+  disabled: true
 });
 
+$('#name').focusout(function(e){ //If name is empty, make border red
+  let nameInput = $('#name').val();
+  if (nameInput == ''){
+    e.preventDefault();
+    $('#mail').prop({
+      disabled: true
+    });
+    $('#name').css('border-color', 'red');
+  }
+});
 
-//Subsequent handlers check validation when form is being filled out.
-//Form inputs should be validated on keyup.
-//And enables register button.
-$('#name').keyup(function(){  
-  $("button:contains('Register')").prop({
-    disabled: false
-  });
+$('#name').keyup(function(){
   let nameInput = $('#name').val();
   if (nameInput != ''){
     $('#name').css('border-color', 'white');
+    $('#mail').prop({
+      disabled: false
+    });
+  }
+});
+
+$('#mail').focusout(function(e){ //If address is invalid, make border red
+  e.preventDefault();         
+  let emailInput = $('#mail').val();
+  let aIndex = emailInput.indexOf('@');
+  let pIndex = emailInput.lastIndexOf('.');
+  if (aIndex < 1 || pIndex < aIndex +2 || pIndex +2 >= emailInput.length) {
+    $('.activities').prop({
+      disabled: true
+    });
+    $('#mail').css('border-color', 'red');
+  }else{
+    $('.activities').prop({
+      disabled: false
+    });
+    $('#mail').css('border-color', '#accbd9');
+    $('.payment').prop({
+      disabled: false
+    });
   }
 });
 
 $('#mail').keyup(function(){
-  $("button:contains('Register')").prop({
-    disabled: false
-  });
   let emailInput = $('#mail').val();
   let aIndex = emailInput.indexOf('@');
   let pIndex = emailInput.lastIndexOf('.');
-  if (aIndex > 1 && pIndex > aIndex +2 && pIndex +3 <= emailInput.length){
+  if (aIndex > 1 && pIndex > aIndex +2 && pIndex +2 <= emailInput.length){
     $('#mail').css('border-color', 'white');
+    $('.activities').prop({
+      disabled: false
+    });
   }
 });
-                          
-$(".activities input[type='checkbox']").click(function() {   
-  $("button:contains('Register')").prop({
-    disabled: false
-  });
+
+$(".activities input[type='checkbox']").click(function() {  //if no activity is selected, make all red 
   if (mainConf.is(':checked') || jsFrameworks.is(':checked') || jsLibs.is(':checked') 
   || express.is(':checked') || node.is(':checked') || buildTools.is(':checked') || npm.is(':checked') ){
-    $('legend span').text('').css('color', '#c13c24');
+    $('.activities label').css('color', 'black');
+    $('.payment').prop({
+      disabled: false
+    });
   }else{
-    $('legend span').text(' Must Choose at Least 1').css('color', '#c13c24');
+    $('.payment').prop({
+      disabled: true
+    });
+    $('.activities label').css('color', 'red');
   }
-}); 
-
-
-$('.default').is(':selected');
-if($('.default').is(':selected')){
-}
+});
 
 $('#payment').change(function(){ 
   let value = $(this).val();
-  if(value === 'paypal'){
-    $("button:contains('Register')").prop({
+  if(value === 'credit card'){
+    $('#cc-num').prop({
       disabled: false
     });
-    $('label span').text('').css('color', '#c13c24');
-  } else if(value === 'bitcoin'){
-    $("button:contains('Register')").prop({
-      disabled: false
-    });    
-    $('label span').text('').css('color', '#c13c24'); 
   }
 });
 
-$('#cc-num').keyup(function(e){ 
+$('#cc-num').focusout(function(e){  //Verifies a valid number, makes input red otherwise
   e.preventDefault();
-  $("button:contains('Register')").prop({
-    disabled: false
-  });
   let val = $('#cc-num').val();
   let valLength = val.length;
-  if (isNaN(val) || val === '' || valLength < 13 || valLength > 16){
+  if (isNaN(val) || val === '' || val < 13 || valLength > 16){
     e.preventDefault();
-    $('#cc-num').css('border-color', '#c13c24');
-  }else if(valLength > 13 && valLength < 16 && isNaN(val) === false){
-    $('#cc-num').css('border-color', 'white');
+    $('#cc-num').css('border-color', 'red');
+    $('#zip').prop({
+      disabled: true
+    });
+  }else{
+    $('.error').text('');
+    $('#zip').prop({
+      disabled: true
+    });
   }
-}); 
+  if(valLength > 13 && valLength < 16 && isNaN(val) === false) {
+    $('#zip').prop({
+      disabled: false
+    });
+    $('#cc-num').css('border-color', '#accbd9');
+  }
+});
 
-$('#zip').keyup(function(){
-  $("button:contains('Register')").prop({
-    disabled: false
-  }); 
+$('#cc-num').keyup(function(){  
+  let val = $('#cc-num').val();
+  let valLength = val.length;
+  if(valLength > 13 && valLength < 16 && isNaN(val) === false) {
+    $('#zip').prop({
+      disabled: false
+    });
+    $('#cc-num').css('border-color', '#accbd9');
+  }
+});
+
+$('#zip').focusout(function(e){  //Verifies a valid number, makes input red otherwise
+  e.preventDefault();
+  let val = $('#zip').val();
+  let valLength = val.length;
+  if (isNaN(val) || valLength != 5){
+    e.preventDefault();
+    $('.validationPay').text('Must be 5 digits long');
+    $('#zip').css('border-color', 'red');
+    $('#cvv').prop({
+      disabled: true
+    });
+  }
+  if(valLength === 5 && isNaN(val) === false) {
+    $('#cvv').prop({
+      disabled: false
+    }); 
+    $('#zip').css('border-color', '#accbd9');
+  }
+});
+
+$('#zip').keyup(function(){ 
   let val = $('#zip').val();
   let valLength = val.length;
   if(valLength === 5 && isNaN(val) === false) {
-    $('#zip').css('border-color', 'white');
-  }else if(valLength != 5 || isNaN(val) ) { 
-    $('#zip').css('border-color', '#c13c24');
+    $('#cvv').prop({
+      disabled: false
+    }); 
+    $('#zip').css('border-color', '#accbd9');
   }
-});
+}); 
 
-$('#cvv').keyup(function(){ 
-  $("button:contains('Register')").prop({
-    disabled: false
-  });
+$('#cvv').focusout(function(e){  //Verifies a valid number, makes input red otherwise
+  e.preventDefault();
   let val = $('#cvv').val();
   let valLength = val.length;
   if (isNaN(val) || valLength != 3){
-    $('#cvv').css('border-color', '#c13c24');
-  } else if(valLength === 3 && isNaN(val) === false) {
-    $('#cvv').css('border-color', 'white');
+    e.preventDefault();
+    $("button:contains('Register')").prop({
+      disabled: true
+    });
+    $('#cvv').css('border-color', 'red');
+
+  }else{
+    $('.error').text('');
+  }
+  if(valLength === 3 && isNaN(val) === false) {
+    $("button:contains('Register')").prop({
+      disabled: false
+    });
   }
 });
 
-$("button:contains('Register')").click(function(){
+$('#cvv').keyup(function(){ //Finally, if CVV is valid allow for a form submit
+  let val = $('#cvv').val();
+  let valLength = val.length;
+  if(valLength === 3 && isNaN(val) === false) {
+    $('#cvv').prop({
+      disabled: false
+    }); 
+    $('#cvv').css('border-color', '#accbd9');
+  }
+  $("button:contains('Register')").prop({
+    disabled: false
+  });
 });
+
